aedc2fb046c76a3ec5dbc7efa5fec4596a720176
Aaron <gitkraken@aaronlcjones.com>
2019-07-09 14:34:41 +1000

    Basic google maps component for testing API & props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import GoogleMapContainer from "./googlemaps"
+
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <BrowserRouter>
+        <header className="App-header">
+        <Link to="/map/">Map</Link>
+        </header>
+        <Route path="/map/" component={GoogleMapContainer} />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/googlemaps.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+
+const MapRender = ({ text }) => <div>{ text }</div>;
+
+const mapStyles = {
+    width: '500px',
+    height: '500px'
+};
+class GoogleMapContainer extends Component {
+    static defaultProps = {
+        center: {lat: -27.4698, lng: 153.0251},
+        zoom: 11
+    };
+render() {
+    return (
+        <div className="maps-wrapper" style={mapStyles}>
+            <div className='formCont'>
+                <form>
+                    <label>Latitude:
+                        <input type="number" name="Latitude" />
+                    </label>
+                    <label>Longitude:
+                        <input type="number" name="Longitude" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key:"AIzaSyCgZJe4oyVbeucoA2e2RvWAK9P8IQgG1Dk" }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                    >
+                    <MapRender
+                        lat={-27.4698} 
+                        lng={153.0251}
+                        text={'Test Map'}
+                    />
+                </GoogleMapReact>
+            </div>            
+            )
+        }
+    }
+
+    export default GoogleMapContainer
+
