f62d23b1f914c0c887ac0b05d85ecb9d465d47d4
Dylan-Speight <d.speight92@gmail.com>
2019-07-12 11:16:41 +1000

    its not broken!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import './Styling/App.css'
+// import './Styling/App.css'
 import NavBar from './Navbar';
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
+
+
+const InvestmentQuery = gql`
+  {
+    investments {
+      id
+      text
+      complete
+    }
+  }
+`;
 function App() {
   return (
     <div>
@@ -10,4 +23,34 @@ function App() {
   )
 }
 
-export default App;
+export default graphql(InvestmentQuery)(App);
+
+
+//Example of calling from the database
+
+// class App extends React.Component {
+//   render() {
+//     const {
+//       data: { loading, investments }
+//     } = this.props;
+//     if (loading) {
+//       return null;
+//     }
+//     return (
+//       <div style={{ display: 'flex' }}>
+//         <div style={{ margin: 'auto', width: 400 }}>
+//         {investments.map(investment => (
+//           <Paper elevation={1}>
+//           <div key={`${investment.id}-investment-item`}>
+//             {investment.text}
+//           </div>
+//         </Paper>
+//         ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+
+// export default graphql(InvestmentQuery)(App);
