ec65bdfe1c9c7bb391f6a0d7e1d2c6cf9ca35ef7
Dylan-Speight <d.speight92@gmail.com>
2019-07-23 14:20:43 +1000

    mag goat implemented2

--- src/components/user/userRegister.jsx
+++ src/components/user/userRegister.jsx
@@ -31,6 +31,7 @@ export default class UserRegister extends Component {
         console.log(JSON.stringify(this.state))
         fetch(`${uri}/api/register`, {
             method: 'POST',
+            mode: 'no-cors',
             body: JSON.stringify(this.state),
             headers: {
               'Content-Type': 'application/json'
