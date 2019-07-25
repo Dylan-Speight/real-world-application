1c908e468bcc668fffc1f939e6478fe0aac00245
Dylan-Speight <d.speight92@gmail.com>
2019-07-25 16:48:48 +1000

    screenshots

--- src/components/user/userLogin.jsx
+++ src/components/user/userLogin.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from 'react'
 import LoggedInContext  from './userContext'
 import cookie from 'react-cookies';
-import { Link } from 'react-router-dom';
-const uri = "https://magnanimous-goat-5432.herokuapp.com";
 import { Container, Field, Label, Control, Input, Help, Button} from 'bloomer'
 import { Link } from 'react-router-dom';
 const uri = "https://magnanimous-goat-5432.herokuapp.com";
